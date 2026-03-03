42444b06db16a62b59ed4538ef479355f005417f
Ryan Davis <rcdavis13@gmail.com>
2026-03-03 18:15:20 -0500

    Initial Commit

---
+++ Since/SinceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinceApp.swift
+//  Since
+//
+//  Created by Ryan Davis on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SinceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
